6d7a39e79f37b712bdbb2fb7e9269fef3d7d5ae0
Xenunxenunowski <blacksidepolska@gmail.com>
2021-08-18 22:40:55 +0200

    stared finishing stuff up

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,12 +1,18 @@
 package sample;
 
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hashing;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import me.xdrop.fuzzywuzzy.FuzzySearch;
-import sample.languageControll.LanguageController;
+
+import javax.xml.bind.DatatypeConverter;
+import java.io.File;
+import java.security.SecureRandom;
+import java.util.Base64;
 
 public class Main extends Application {
 
@@ -20,8 +26,25 @@ public class Main extends Application {
         Scene mainScene = primaryStage.getScene();
         primaryStage.show();
         System.out.println(FuzzySearch.weightedRatio("wHoLeSomeCraft","WholesomeCraft Lite"));
-    }
 
+        final byte[] sample = new byte[131];
+
+        new SecureRandom().nextBytes(sample);
+
+        System.out.println("Sample: " + DatatypeConverter.printHexBinary(sample));
+        HashCode hashCode = Hashing.sha256()
+                .hashBytes(sample);
+
+        String encodedURL = Base64.getUrlEncoder()
+                .encodeToString(hashCode.asBytes());
+        System.out.println("SHA256: " +encodedURL);
+        SettingsControler settingsControler = new SettingsControler();
+        settingsControler.loadSettings();
+        settingsControler.saveSettings();
+
+        System.out.println(new File("/config.properties"));
+
+    }
 
     public static void main(String[] args) {
         launch(args);
